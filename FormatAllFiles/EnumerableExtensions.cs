b134312600bf79577eb510c5af783ac867b35c72
munyabe <yo.betek@gmail.com>
2016-02-18 19:40:39 +0900

    Implement a command to format all files.

---
+++ FormatAllFiles/EnumerableExtensions.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace FormatAllFiles
+{
+    /// <summary>
+    /// <see cref="IEnumerable{T}"/>の拡張メソッドを定義するクラスです。
+    /// </summary>
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// <see cref="IEnumerable{T}"/>の各要素に対して、指定された処理を実行します。
+        /// </summary>
+        /// <typeparam name="T">各要素の型</typeparam>
+        /// <param name="source">処理を適用する値のシーケンス</param>
+        /// <param name="action">各要素に対して実行するアクション</param>
+        /// <exception cref="ArgumentNullException"><paramref name="source"/>または<paramref name="action"/>が<see langword="null"/>です。</exception>
+        [DebuggerStepThrough]
+        public static void ForEach<T>(this IEnumerable<T> source, Action<T> action)
+        {
+            ArgumentNotNull(source, "source");
+            ArgumentNotNull(action, "action");
+
+            foreach (T each in source)
+            {
+                action(each);
+            }
+        }
+
+        /// <summary>
+        /// <see cref="IEnumerable{T}"/>の各要素と子要素を幅優先探索で再帰的に列挙します。
+        /// </summary>
+        /// <typeparam name="T">各要素の型</typeparam>
+        /// <param name="source">処理を適用する値のシーケンス</param>
+        /// <param name="getChildren">親要素から子要素の集合を取得する処理</param>
+        /// <returns>走査する親要素</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="getChildren"/>が<see langword="null"/>です。</exception>
+        [DebuggerStepThrough]
+        public static IEnumerable<T> Recursive<T>(this IEnumerable<T> source, Func<T, IEnumerable<T>> getChildren)
+        {
+            ArgumentNotNull(source, "source");
+            ArgumentNotNull(getChildren, "getChildren");
+
+            foreach (var item in source)
+            {
+                yield return item;
+            }
+            foreach (var item in source)
+            {
+                var results = SearchBreadthFirst(item, getChildren);
+                foreach (var result in results)
+                {
+                    yield return result;
+                }
+            }
+
+            yield break;
+        }
+
+        /// <summary>
+        /// パラメーターが<see langword="null"/>でないことを示します。
+        /// </summary>
+        /// <typeparam name="T">パラメーターの型</typeparam>
+        /// <param name="argumentValue">チェックするパラメーター</param>
+        /// <param name="argumentName">チェックするパラメーター名</param>
+        /// <exception cref="ArgumentNullException"><paramref name="argumentValue"/>が<see langword="null"/>です。</exception>
+        private static void ArgumentNotNull<T>(T argumentValue, string argumentName) where T : class
+        {
+            if (argumentValue == null)
+            {
+                throw new ArgumentNullException(argumentName);
+            }
+        }
+
+        /// <summary>
+        /// グラフ構造を幅優先探索します。
+        /// </summary>
+        private static IEnumerable<T> SearchBreadthFirst<T>(T source, Func<T, IEnumerable<T>> getChildren)
+        {
+            if (source == null)
+            {
+                yield break;
+            }
+
+            var queue = new Queue<T>();
+            Action<T> addChild = item =>
+            {
+                var children = getChildren(item);
+                if (children != null)
+                {
+                    children.ForEach(queue.Enqueue);
+                }
+            };
+
+            addChild(source);
+
+            while (queue.Any())
+            {
+                var current = queue.Dequeue();
+                T target = current;
+                if (target != null)
+                {
+                    yield return target;
+                }
+
+                addChild(current);
+            }
+        }
+    }
+}
--- FormatAllFiles/FormatAllFilesCommand.cs
+++ FormatAllFiles/FormatAllFilesCommand.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using EnvDTE;
+using Microsoft.VisualStudio;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
 
 namespace FormatAllFiles
 {
@@ -14,6 +20,11 @@ namespace FormatAllFiles
         public const int CommandId = 0x0100;
 
         /// <summary>
+        /// ドキュメントをフォーマットするコマンドです。
+        /// </summary>
+        private const string FORMAT_DOCUMENT_COMMAND = "Edit.FormatDocument";
+
+        /// <summary>
         /// コマンドメニューグループのIDです。
         /// </summary>
         public static readonly Guid CommandSet = new Guid("b9f80962-1b6d-4cfb-bcd8-bd51f716e103");
@@ -22,6 +33,27 @@ namespace FormatAllFiles
         /// シングルトンのインスタンスを取得します。
         /// </summary>
         public static FormatAllFilesCommand Instance { get; private set; }
+
+        private IVsOutputWindowPane _outputWindowPane;
+        /// <summary>
+        /// 出力ウィンドウのペインを取得します。
+        /// </summary>
+        private IVsOutputWindowPane OutputWindowPane
+        {
+            get
+            {
+                if (_outputWindowPane == null)
+                {
+                    var outputWindow = Package.GetGlobalService(typeof(SVsOutputWindow)) as IVsOutputWindow;
+
+                    var guidGeneral = VSConstants.OutputWindowPaneGuid.GeneralPane_guid;
+                    outputWindow.CreatePane(guidGeneral, "FormatAllFiles", 1, 1);
+                    outputWindow.GetPane(guidGeneral, out _outputWindowPane);
+                }
+
+                return _outputWindowPane;
+            }
+        }
 
         /// <summary>
         /// インスタンスを初期化します。
@@ -43,6 +75,84 @@ namespace FormatAllFiles
         /// <inheritdoc />
         protected override void Execute(object sender, EventArgs e)
         {
+            var dte = ServiceProvider.GetService(typeof(DTE)) as DTE;
+
+            dte.StatusBar.Text = "Format All Files is started.";
+            OutputWindowPane.Clear();
+            WriteOutputWindow(DateTime.Now.ToString("T") + " Started.");
+
+            GetProjectItems(dte.Solution)
+                .Where(item => item.Kind == VSConstants.ItemTypeGuid.PhysicalFile_string)
+                .ForEach(item =>
+                {
+                    var name = item.FileCount != 0 ? item.FileNames[0] : item.Name;
+                    WriteOutputWindow("Formatting: " + name);
+
+                    ExecuteCommand(item, FORMAT_DOCUMENT_COMMAND);
+                });
+
+            WriteOutputWindow(DateTime.Now.ToString("T") + " Finished.");
+            dte.StatusBar.Text = "Format All Files is finished.";
+        }
+
+        /// <summary>
+        /// プロジェクトのアイテムを開いて指定のコマンドを実行します。
+        /// </summary>
+        private void ExecuteCommand(ProjectItem item, string command)
+        {
+            var isOpen = item.get_IsOpen();
+            if (isOpen == false)
+            {
+                item.Open(VSConstants.LOGVIEWID.TextView_string);
+            }
+            var document = item.Document;
+            if (document != null)
+            {
+                try
+                {
+                    document.Activate();
+                    item.DTE.ExecuteCommand(command);
+                }
+                catch (COMException ex)
+                {
+                    WriteOutputWindow(ex.Message);
+                }
+                finally
+                {
+                    if (isOpen)
+                    {
+                        document.Save();
+                    }
+                    else
+                    {
+                        document.Close(vsSaveChanges.vsSaveChangesYes);
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// ソリューションに含まれるアイテムの一覧を取得します。
+        /// </summary>
+        private IEnumerable<ProjectItem> GetProjectItems(Solution solution)
+        {
+            return solution.Projects.OfType<Project>()
+                .SelectMany(x => x.ProjectItems.OfType<ProjectItem>())
+                .Recursive(x =>
+                {
+                    var innerItems = x.ProjectItems;
+                    return innerItems != null && innerItems.Count != 0 ? innerItems.OfType<ProjectItem>() : Enumerable.Empty<ProjectItem>();
+                });
+        }
+
+        /// <summary>
+        /// 出力ウィンドウにメッセージを出力します。
+        /// </summary>
+        private void WriteOutputWindow(string message)
+        {
+            var pane = OutputWindowPane;
+            pane.OutputString(message);
+            pane.OutputString(Environment.NewLine);
         }
     }
 }
