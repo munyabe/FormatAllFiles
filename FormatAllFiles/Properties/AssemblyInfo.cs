6e3bc916a0e53d2fbd9afd026509a12e288a6033
munyabe <yo.betek@gmail.com>
2016-02-24 13:02:27 +0900

    Add a test project.

--- FormatAllFiles/Properties/AssemblyInfo.cs
+++ FormatAllFiles/Properties/AssemblyInfo.cs
@@ -31,3 +31,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
+#if DEBUG
+[assembly: InternalsVisibleTo("Test.FormatAllFiles, PublicKey=0024000004800000940000000602000000240000525341310004000001000100b7aa23d0ca1936e8f01fcb1747da8024a41a8119d59de7122f0f7621453923b85aaa8925c1ec6c33eae9acd9ac501b55650568c5b7378f91f1cdcd725936ad5e2fcea93be88660631cc6acb2703d50b373e54f68ba128cc7b9fbbda27020922b73b585c271235bc605e97f29240b662f1436f33cef48b7b4d10ec0a94ddfd5e0")]
+#endif
